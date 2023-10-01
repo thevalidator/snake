6cbf6fa38aa6fb2f99ef5cb6b7272fca3b11a451
illusion <the.validator@yandex.ru>
2023-10-01 14:59:45 +0300

    init commit

---
+++ src/main/java/ru/thevalidator/snake/Snake.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.snake;
+
+/**
+ *
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Snake {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
